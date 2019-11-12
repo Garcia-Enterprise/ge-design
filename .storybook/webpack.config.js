4f20ec058bfc8646851676aea59fd7aa3bff7957
Eric N. Garcia <engarcia618@gmail.com>
2019-11-12 15:51:48 -0500

    Adds storybook and sourcecode addon

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = function({ config }) {
+  config.module.rules.unshift({
+    test: /\.stories\.jsx?$/,
+    loaders: [require.resolve('@storybook/source-loader')],
+    enforce: 'pre',
+  });
+
+  return config;
+};
