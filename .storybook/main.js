9c7bcb5880664a5182665ec903a516a1e349ab96
Eric N. Garcia <eng618@garciaericn.com>
2021-03-16 10:32:26 -0400

    feat: migrate storybook config to latest

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  stories: ['../**/*.stories.js'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+      },
+    },
+  ]
+};
