7aae335d4583ac62a59c294fb0562123f771828e
Eric N. Garcia <eng618@garciaericn.com>
2021-04-22 10:41:54 -0400

    build: update dependencies
    * refactors ESLint to its own file rather than the package.json
    * Changes badge to github actions from circleci

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: [
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['jsx-a11y', 'react', 'react-hooks'],
+  ignorePatterns: ['storybook-static/', 'node_modules/'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
